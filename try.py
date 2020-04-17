01eefe0924d156446bd43ebe372735dd2a85ca36
Martin Johnson <mbrad@synopsys.com>
2020-04-17 14:24:31 +0100

    Added zip entry crc checking for large file dups

--- try.py
+++ try.py
@@ -27,32 +27,23 @@ arc_files_dict = {}
 
 #tempdir = tempfile.mkdtemp()
 
-# EXAMPLE EXTRACT ZIP FROM ZIP
-# with zipfile.ZipFile('zip2.zip') as z:
-# 	with z.open('zip1.zip') as z2:
-# 		z2_filedata =  io.BytesIO(z2.read())
-# 		with zipfile.ZipFile(z2_filedata) as nested_zip:
-# 			print( nested_zip.open('try.py').read())
-
 def process_zip(name, path, depth, zipinfo):
 #
 # zipinfo is a zipinfo entry - if None then file should be processed otherwise process zipinfo
-	print("Processing {}".format(path))
+#	print("Processing {}".format(path))
 	depth += 1
 	if zipinfo == None:
 		with zipfile.ZipFile(path, 'r') as zf:
 			for zinfo in zf.infolist():
 				fullpath = path + "##" + zinfo.filename
-				print(fullpath)
-				if zinfo.is_dir():
-					print("dir")
+#				print(fullpath)
+#				if zinfo.is_dir():
+#					print("dir")
 				arc_files_dict[fullpath] = zinfo.CRC
+				checkfile(zinfo.filename, fullpath, zinfo.file_size, depth)
 				dot = zinfo.filename.rfind(".")
 				if dot > 1:
 					if zinfo.filename[dot:] == ".zip":
-# 						zf.extract(zinfo, os.path.join(tempdir, newzpath))
-# 						print(os.path.join(tempdir, newzpath))
-# 						process_zip(zinfo.filename, fullpath, depth)
 						with zf.open(zinfo.filename) as z2:
 							z2_filedata =  io.BytesIO(z2.read())
 							try:
@@ -61,18 +52,16 @@ def process_zip(name, path, depth, zipinfo):
 									process_zip(zinfo.filename, fullpath, depth, nested_zip)
 # 									print( nested_zip.open('try.py').read())
 							except:
-								print("ERROR: Can't open zip")
-								return
+								print("WARNING: Can't open zip {} (Skipped)".format(fullpath))
+								continue
 	else:
 		for zinfo in zipinfo.infolist():
 			fullpath = path + "##" + zinfo.filename
 			arc_files_dict[fullpath] = zinfo.CRC
+			checkfile(zinfo.filename, fullpath, zinfo.file_size, depth)
 			dot = zinfo.filename.rfind(".")
 			if dot > 1:
 				if zinfo.filename[dot:] == ".zip":
-# 						zf.extract(zinfo, os.path.join(tempdir, newzpath))
-# 						print(os.path.join(tempdir, newzpath))
-# 						process_zip(zinfo.filename, fullpath, depth)
 					try:
 						with z.open(zinfo.filename) as z2:
 							z2_filedata =  io.BytesIO(z2.read())
@@ -81,8 +70,8 @@ def process_zip(name, path, depth, zipinfo):
 								process_zip(zinfo.filename, fullpath, depth, nested_zip)
 # 								print( nested_zip.open('try.py').read())
 					except:
-						print("ERROR: Can't open zip")
-						return
+						print("ERROR: Can't open zip {} (Skipped)".format(fullpath))
+						continue
 
 def checkfile(name, path, size, depth):
 	global size_total
@@ -106,8 +95,6 @@ def checkfile(name, path, size, depth):
 		elif name[dot:] in arcext_list:
 			arc_list.append(path)
 			#print("archive")
-			if name[dot:] == '.zip':
-				process_zip(name, path, depth, None)
 		else:
 			#print("other")
 			other_list.append(path)
@@ -119,7 +106,7 @@ def checkfile(name, path, size, depth):
 		size_huge += size
 		large_dict[path] = size
 		#print("huge")
-	elif size > 500000:
+	elif size > 1000000:
 		large_list.append(path)
 		size_large += size
 		large_dict[path] = size
@@ -140,6 +127,10 @@ def process_dir(path, depth):
 			dir_size += process_dir(entry.path, depth)
 		else:
 			checkfile(entry.name, entry.path, entry.stat(follow_symlinks=False).st_size, depth)
+			dot = entry.name.rfind(".")
+			if entry.name[dot:] == '.zip':
+				process_zip(entry.name, entry.path, depth, None)
+
 			dir_size += entry.stat(follow_symlinks=False).st_size
 	dir_dict[path]['num_entries'] = dir_entries
 	dir_dict[path]['size'] = dir_size
@@ -151,33 +142,38 @@ def process_largefiledups():
 	import filecmp
 	total_dup_size = 0
 	count_dups = 0
-	for path, size in large_dict.items():
+	for apath, asize in large_dict.items():
 		dup = False
 		for cpath, csize in large_dict.items():
-			if path == cpath:
+			if apath == cpath:
 				continue
-			if size == csize:
-				dot = path.rfind(".")
+			if asize == csize:
+				adot = apath.rfind(".")
 				cdot = cpath.rfind(".")
-				if (dot > 1) and (cdot > 1):
-					if path[dot:] == cpath[cdot:]:
+				if (adot > 1) and (cdot > 1):
+					if apath[adot:] == cpath[cdot:]:
 						dup = True
-				elif (dot == 0) and (cdot == 0):
+				elif (adot == 0) and (cdot == 0):
 					dup = True
 				if dup:
-					if path.find("##") > 0:
-						if arc_files_dict.get(path) != None:
-							test = get_crc(cpath) and arc_files_dict[path]
-					elif cpath.find("##") > 0:
-						if arc_files_dict.get(cpath) != None:
-							test = get_crc(path) and arc_files_dict[cpath]						
+					if apath.find("##") > 0 or cpath.find("##") > 0:
+						if apath.find("##") > 0:
+							acrc = arc_files_dict[apath]
+						else:
+							acrc = get_crc(apath)
+						if cpath.find("##") > 0:
+							ccrc = arc_files_dict[cpath]
+						else:
+							ccrc = get_crc(cpath)
+						test = (acrc == ccrc)
 					else:
-						test = filecmp.cmp(path, cpath, True)
-					if test and dup_large_dict.get(cpath) == None and dup_dir_dict.get(os.path.dirname(path)) == None and dup_dir_dict.get(os.path.dirname(cpath)) == None:
-						dup_large_dict[path] = cpath
-						total_dup_size += size
+						test = filecmp.cmp(apath, cpath, True)
+
+					if test and dup_large_dict.get(cpath) == None and dup_dir_dict.get(os.path.dirname(apath)) == None and dup_dir_dict.get(os.path.dirname(cpath)) == None:
+						dup_large_dict[apath] = cpath
+						total_dup_size += asize
 						count_dups += 1
-						print("- Dup large file - {}, {} (size {}MB)".format(path,cpath,trunc(size/1000000)))
+						print("- Dup large file - {}, {} (size {}MB)".format(apath,cpath,trunc(asize/1000000)))
 	return(count_dups, total_dup_size)
 
 def process_dirdups():
@@ -195,10 +191,9 @@ def process_dirdups():
 			continue
 		for cpath, cdict in dir_dict.items():
 			if apath != cpath:
-				print(apath, cpath)
+#				print(apath, cpath)
 				if adict['num_entries'] == cdict['num_entries'] and adict['size'] == cdict['size'] and adict['filenamesstring'] == cdict['filenamesstring']:
 					if not (dup_dir_dict.get(apath) == cpath or dup_dir_dict.get(cpath) == apath):
-						print("dup")
 						#
 						# Need to check whether this dupdir exists or can replace existing entries (is higher up)
 						keydir = ""
@@ -224,7 +219,7 @@ def process_dirdups():
 							count_dupdirs += 1
 							size_dupdirs += adict['size']
 	print(" OK\n- {} Duplicate folders identified".format(count_dupdirs))
-	
+
 	return(count_dupdirs, size_dupdirs)
 
 def check_singlefiles():
@@ -251,9 +246,9 @@ def check_singlefiles():
 					sfmatch = True
 					sf_list.append(thisfile)
 	if sfmatch:
-		print("- Consider using Single file matching - singleton .js files found:")
-		for thisfile in sf_list:
-			print("    {}".format(thisfile))
+		print("- Consider using Single file matching - {} singleton .js files found".format(len(sf_list)))
+# 		for thisfile in sf_list:
+# 			print("    {}".format(thisfile))
 
 def get_crc(myfile):
 	import zlib
@@ -286,7 +281,7 @@ print("- Binary files = {}".format(len(bin_list)))
 print("- Jar files = {}".format(len(jar_list)))
 print("- Archive files = {}".format(len(arc_list)))
 print("- Other files = {}".format(len(other_list)))
-print("- Large files (>500KB) = {} (Total size {}MB)".format(len(large_list), trunc(size_large/1000000)))
+print("- Large files (>1MB) = {} (Total size {}MB)".format(len(large_list), trunc(size_large/1000000)))
 print("- Huge files (>10MB) = {} (Total size {}MB)".format(len(huge_list), trunc(size_huge/1000000)))
 
 print("\nDUPLICATES:")
